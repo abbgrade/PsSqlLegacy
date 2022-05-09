44dff5b1d40e5fa1f01fdd5ab96f9a2be828165e
Steffen <skampmann@munichre.com>
2022-05-09 16:45:09 +0200

    extendted module test

--- Test/Module.Tests.ps1
+++ Test/Module.Tests.ps1
@@ -1,5 +1,20 @@
 Describe PsPowerBi {
-    It is-valid {
+    It 'is valid' {
         Test-ModuleManifest $PSScriptRoot\..\Source\PsSqlLegacy.psd1
+    }
+
+    It 'can be imported' {
+        Import-Module $PSScriptRoot\..\Source\PsSqlLegacy.psd1 -Force
+    }
+
+    Context 'loaded modules' {
+        BeforeAll {
+            Import-Module $PSScriptRoot\..\Source\PsSqlLegacy.psd1 -Force -Verbose
+        }
+
+        It 'has commands' {
+            $commands = Get-Command -Module 'PsSqlLegacy'
+            $commands | Should -Not -BeGreaterOrEqual
+        }
     }
 }
