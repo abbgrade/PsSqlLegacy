92408e42d79f138890e010475b8323573d1d552c
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-08-21 12:20:52 +0200

    updated tests

--- tasks/Testdata.Tasks.ps1
+++ tasks/Testdata.Tasks.ps1
@@ -1,3 +1,5 @@
+. $PSScriptRoot/PsSqlTestTasks/SqlServerSamples.Tasks.ps1
 . $PSScriptRoot/PsSqlTestTasks/WideWorldImporters.Tasks.ps1
+. $PSScriptRoot/PsSqlTestTasks/AdventureWorks.Tasks.ps1
 
-task Testdata.Create -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.Create
+task Testdata.Create -Jobs PsSqlTestTasks.AdventureWorks.CheckoutDirectory, PsSqlTestTasks.WideWorldImporters.DacPac.Create
--- test/Invoke-SqlPackage.Tests.ps1
+++ test/Invoke-SqlPackage.Tests.ps1
@@ -5,24 +5,33 @@ Describe Invoke-SqlPackage {
     BeforeAll {
         Import-Module $PSScriptRoot\..\src\PsSqlLegacy.psd1 -Force
         Initialize-LegacySqlPackage -Path 'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\SqlPackage.exe'
-
-        $ServerInstance = '(localdb)\MSSQLLocalDB'
-        $DatabaseName = 'AzureStorageEmulatorDb510'
     }
 
-    it 'Extract' {
-        Invoke-LegacySqlPackage -Extract `
-            -SourceServerName $ServerInstance `
-            -SourceDatabaseName $DatabaseName `
-            -DacPac ./test.dacpac `
-            -ErrorAction Stop
+    It Exists {
+        Get-Command -Name Invoke-LegacySqlCmd -Module PsSqlLegacy | Should -Not -BeNullOrEmpty
     }
 
-    it 'Publish' {
-        Invoke-LegacySqlPackage -Publish `
-            -TargetServerName $ServerInstance `
-            -TargetDatabaseName "$DatabaseName-neu" `
-            -DacPac ./test.dacpac `
-            -ErrorAction Stop
+    Context Database {
+
+        BeforeAll {
+            $ServerInstance = '(localdb)\MSSQLLocalDB'
+            $DatabaseName = 'AzureStorageEmulatorDb510'
+        }
+
+        It Extracts {
+            Invoke-LegacySqlPackage -Extract `
+                -SourceServerName $ServerInstance `
+                -SourceDatabaseName $DatabaseName `
+                -DacPac ./test.dacpac `
+                -ErrorAction Stop
+        }
+
+        It Publish {
+            Invoke-LegacySqlPackage -Publish `
+                -TargetServerName $ServerInstance `
+                -TargetDatabaseName "$DatabaseName-neu" `
+                -DacPac ./test.dacpac `
+                -ErrorAction Stop
+        }
     }
 }
