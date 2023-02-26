980e329736cf44bc4b716af73ea9e5d9053f7d5c
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-02-26 11:04:54 +0100

    updated psbuildtasks and added readme

---
+++ README.md
@@ -0,0 +1,59 @@
+# Readme
+
+PsSqlLegacy is a PowerShell wrapper for the SQL Server tools SqlCmd and SqlPackage.
+
+For details, visit the [GitHub Pages](https://abbgrade.github.io/PsSqlLegacy/).
+
+## Usage
+
+See [the command reference](./docs/) for descriptions and examples.
+
+## Changelog
+
+See the [changelog](./CHANGELOG.md) file.
+
+## Installation
+
+This module can be installed from [PsGallery](https://www.powershellgallery.com/packages/PsSqlLegacy).
+
+```powershell
+Install-Module -Name PsSqlLegacy -AllowPrerelease
+```
+
+Alternatively it can be built and installed from the sources:
+
+1. Install the development dependencies
+2. Download or clone it from GitHub
+3. Run the installation task:
+
+```powershell
+Invoke-Build Install
+```
+
+## Development
+
+- This is a [Portable Module](https://docs.microsoft.com/de-de/powershell/scripting/dev-cross-plat/writing-portable-modules?view=powershell-7).
+- [VSCode](https://code.visualstudio.com) is recommended as IDE. [VSCode Tasks](https://code.visualstudio.com/docs/editor/tasks) are configured.
+- Build automation is based on [InvokeBuild](https://github.com/nightroman/Invoke-Build)
+- Test automation is based on [Pester](https://pester.dev)
+- Commands are named based on [Approved Verbs for PowerShell Commands](https://docs.microsoft.com/de-de/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands)
+- This project uses [git-flow](https://github.com/nvie/gitflow).
+- This project uses [keep a changelog](https://keepachangelog.com/en/1.0.0/).
+
+### Build
+
+The build scripts require InvokeBuild. If it is not installed, install it with the command `Install-Module InvokeBuild -Scope CurrentUser`.
+
+You can build the module using the VS Code build task or with the command `Invoke-Build Build`.
+
+### Release
+
+1. Create a release branch using git-flow.
+2. Update the version number in the module manifest.
+3. Extend the changelog in this readme.
+4. If you want to create a pre-release.
+   1. Push the release branch to GitHub, to publish the pre-release to PsGallery.
+5. Finish release using git-flow.
+6. Check if tags are not pushed to GitHub.
+7. Check if the release branch is deleted on GitHub.
+8. Create the release on GitHub.
--- tasks/PsBuild.Tasks.ps1
+++ tasks/PsBuild.Tasks.ps1
@@ -1,8 +1,13 @@
+
+if ( -Not $PsBuildTaskBranch ) {
+    $PsBuildTaskBranch = 'main'
+}
+
 #region InvokeBuild
 
 task UpdateBuildTasks {
 	Invoke-WebRequest `
-		-Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/Powershell/Build.Tasks.ps1' `
+		-Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/Powershell/Build.Tasks.ps1" `
 		-OutFile "$PSScriptRoot\Build.Tasks.ps1"
 }
 
@@ -13,7 +18,15 @@ task UpdateValidationWorkflow {
     [System.IO.FileInfo] $file = "$PSScriptRoot/../.github/workflows/build-validation.yml"
     New-Item -Type Directory $file.Directory -ErrorAction SilentlyContinue
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/build-validation-matrix.yml' `
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/GitHub/build-validation-matrix.yml" `
+        -OutFile $file
+}
+
+task UpdatePagesWorkflow {
+    [System.IO.FileInfo] $file = "$PSScriptRoot/../.github/workflows/build-pages.yml"
+    New-Item -Type Directory $file.Directory -ErrorAction SilentlyContinue
+    Invoke-WebRequest `
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/GitHub/build-pages.yml" `
         -OutFile $file
 }
 
@@ -22,7 +35,7 @@ task UpdatePreReleaseWorkflow {
     [System.IO.FileInfo] $file = "$PSScriptRoot\..\.github\workflows\pre-release.yml"
     New-Item -Type Directory $file.Directory -ErrorAction SilentlyContinue
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/pre-release-windows.yml' |
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/GitHub/pre-release-windows.yml" |
     ForEach-Object { $_ -replace 'MyModuleName', $ModuleName } |
     Out-File $file -NoNewline
 }
@@ -32,10 +45,12 @@ task UpdateReleaseWorkflow {
     [System.IO.FileInfo] $file = "$PSScriptRoot\..\.github\workflows\release.yml"
     New-Item -Type Directory $file.Directory -ErrorAction SilentlyContinue
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/release-windows.yml' |
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/GitHub/release-windows.yml" |
     ForEach-Object { $_ -replace 'MyModuleName', $ModuleName } |
     Out-File $file -NoNewline
 }
+
+task UpdateWorkflows -Jobs UpdateValidationWorkflow, UpdatePagesWorkflow, UpdatePreReleaseWorkflow, UpdateReleaseWorkflow
 
 #endregion
 #region GitHub Pages
@@ -43,20 +58,22 @@ task UpdateReleaseWorkflow {
 task UpdateIndexPage {
     New-Item -Type Directory "$PSScriptRoot\..\docs" -ErrorAction SilentlyContinue
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/docs/index.md' `
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/docs/index.md" `
         -OutFile "$PSScriptRoot\..\docs\index.md"
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/docs/_config.yml' `
-        -OutFile "$PSScriptRoot\..\docs\_config.yml"
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/_config.yml" `
+        -OutFile "$PSScriptRoot\..\_config.yml"
 }
 
 #endregion
 #region VsCode
 
 task UpdateVsCodeTasks {
+    [System.IO.FileInfo] $file = "$PSScriptRoot\..\.vscode\tasks.json"
+    New-Item -Type Directory $file.Directory -ErrorAction SilentlyContinue
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/VsCode/tasks.json' `
-        -OutFile "$PSScriptRoot\..\.vscode\tasks.json"
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/VsCode/tasks.json" `
+        -OutFile $file
 }
 
 #endregion
@@ -64,10 +81,10 @@ task UpdateVsCodeTasks {
 
 task UpdatePsBuildTasksTasks {
     Invoke-WebRequest `
-        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/tasks/PowerShell-Matrix.Tasks.ps1' `
+        -Uri "https://raw.githubusercontent.com/abbgrade/PsBuildTasks/$PsBuildTaskBranch/tasks/PowerShell-Matrix.Tasks.ps1" `
         -OutFile "$PSScriptRoot\PsBuild.Tasks.ps1"
 }
 
 #endregion
 
-task UpdatePsBuildTasks -Jobs UpdateBuildTasks, UpdateValidationWorkflow, UpdatePreReleaseWorkflow, UpdateIndexPage, UpdateReleaseWorkflow, UpdateVsCodeTasks, UpdatePsBuildTasksTasks
+task UpdatePsBuildTasks -Jobs UpdateBuildTasks, UpdateWorkflows, UpdateIndexPage, UpdateVsCodeTasks, UpdatePsBuildTasksTasks
